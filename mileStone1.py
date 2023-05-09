1997e9a3f0d996a13d54d8562f8ca6db40f910c5
Levi Arias Ramirez <leviar@circinus-46.ics.uci.edu>
2023-05-09 11:42:10 -0700

    tokenizer, openFileURL, urlID, & addInvIndex first drafts

--- mileStone1.py
+++ mileStone1.py
@@ -7,11 +7,15 @@ import shelve
 from urllib.parse import urljoin
 import hashlib
 from simhash import Simhash
-from nltk.stem import PorterStemmer
+from nltk.stem import PorterStemmer # to stem
 from nltk.tokenize import sent_tokenize, word_tokenize
+
+invInd = {} #dictionary of dictionaries
 
 # tokenizer taken from last assingment
 # NEED TO CHANGE FOR THIS ASSIGNMENT'S REQUIREMNTS: ex: U.S.A --> USA (plus more fixes)
+# Don't need to remove stop words
+# Need to stem the words before adding them to tokens (ex: swam --> swim, swimming --> swim), use PorterStemmer
 def tokenizer(page_text_content):
     ''' 
     tokenizer: Takes the page_text_content returned by BeautifulSoup (as a string) and parses this text into tokens.
@@ -38,3 +42,33 @@ def tokenizer(page_text_content):
     return tokens
 
 
+# open the .json file and read the HTML: Mehmet, do this, (libraries: BeautifulSoup, json), I'm thinking return the text content
+# also, we will have to figure out how to deal with broken HTML (BeautifulSoup might handle it)
+def openFileURL(fileName):
+
+
+
+def urlID(url):
+    return hash(url) #not 100% sure this works
+
+# colin, if you can plz address my comments below. we can discuss them later
+def addInvIndex(textContent, url):
+    urlID = urlID(url) #get url ID
+    #is adding it to a dictionary already placing the URL in sorted order?
+    for token in textContent:
+      if token in invInd: # it is a token in the dictionary
+        #would a dictionary make sense here, or would a list make more sense?
+        #my only concern is that would a dictionary place it in sorted order?
+        if urlID in invInd[token]: 
+          invInd[token][urlID] += 1 #if urlID is in invInd[token], incriment its counter
+        else:
+          invInd[token][urlID] = 1 #if urlID is not invInd[token], add it and set it's val to 1
+      else: #token is not in the dictionary
+        invInd[token] = {urlID : 1} #ex: "hello" = {1904984, 1}
+        
+
+
+
+
+
+    
