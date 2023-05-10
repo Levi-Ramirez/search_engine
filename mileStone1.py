4f72ed99ae91ef7e3d98f09162e555a1786a54e9
ColeHajek <coledhajek@gmail.com>
2023-05-10 15:04:20 -0700

    changed tonkenizer function to include stopwords and ignore special characters

--- mileStone1.py
+++ mileStone1.py
@@ -15,6 +15,8 @@ invInd = {} #dictionary of dictionaries
 # tokenizer taken from last assingment
 # NEED TO CHANGE FOR THIS ASSIGNMENT'S REQUIREMNTS: ex: U.S.A --> USA (plus more fixes)
 # Don't need to remove stop words
+# cases: apostraphies, hyphens, periods, 
+# becareful of periods at the end of sentences
 # Need to stem the words before adding them to tokens (ex: swam --> swim, swimming --> swim), use PorterStemmer
 def tokenizer(page_text_content):
     ''' 
@@ -22,29 +24,24 @@ def tokenizer(page_text_content):
     - Tokens are a list of strings who's length that is greater than 1.
     '''
     tokens = []
-    
+    prevchar = ""
+    nextChar = ""
     cur_word = ""
     for ch in page_text_content: #read line character by character
-        if ch in 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890': #check if character is in english alphabet or a number
+
+        if ch in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890.-'": #check if character is in english alphabet or a number
+            if ch in ".-'":
+               continue
             cur_word += ch.lower() #convert that ch to lower case and add it to the cur_word
-        elif cur_word in stop_words:
+        elif len(cur_word) > 0:
+            tokens.append(cur_word)
             cur_word = ""
-        # if it is not a stop_word && is not alphanumeric && the length is greater than one, then we can append it to the list
-        elif len(cur_word) > 1: #we do not want single charecters. for example James's would give us "James" and "s" if we dont do this 
-            tokens.append(cur_word) # add cur word to token list 
-            cur_word = "" #reset cur_word
-        else:
-            cur_word = ''
-    
-    # if cur_word is not empty, we need to add it to the list bc we do not wanna skip the last word unadded
-    if len(cur_word) > 1 and cur_word not in stop_words: 
-        tokens.append(cur_word)
     return tokens
 
 
 # open the .json file and read the HTML: Mehmet, do this, (libraries: BeautifulSoup, json), I'm thinking return the text content
 # also, we will have to figure out how to deal with broken HTML (BeautifulSoup might handle it)
-def openFileURL(fileName):
+#def openFileURL(fileName):
 
 
 
