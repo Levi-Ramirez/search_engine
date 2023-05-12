5a17a3eb39223dc195723a03efebe3cc99878b72
Mehmet Nadi <mhmtalperennadi@gmail.com>
2023-05-11 21:57:40 -0700

    create report

--- mileStone1.py
+++ mileStone1.py
@@ -12,6 +12,7 @@ from simhash import Simhash
 from nltk.stem import PorterStemmer  # to stem
 from nltk.tokenize import sent_tokenize, word_tokenize
 from stop_words import get_stop_words
+import shelve
 
 
 '''
@@ -21,6 +22,10 @@ from stop_words import get_stop_words
 4. create report
 
 '''
+
+docID = 0
+inverted_index = {}
+docID_urls = {}
 
 
 class Posting:
@@ -90,16 +95,13 @@ def get_file_paths(folder_path):
     return paths
 
 
-inverted_index = {}
-docID_urls = {}
-
-
 def generate_inverted_index(count_tokens, docID):
     '''this function generates/fills the inverted_index. Gets count_tokes and docID as parameters.'''
 
     try:
         for token in count_tokens:
-            tfidf = round(count_tokens[token] / len(count_tokens), 4)
+            # tfidf = round(count_tokens[token] / len(count_tokens), 4)
+            tfidf = round(count_tokens[token])
             post = Posting(docID, tfidf)
             if token in inverted_index:
                 inverted_index[token].append(post)
@@ -127,27 +129,44 @@ def generate_report():
     '''This funciton generates our report for milestone 1. It will print the word and the list of all the documents that word seen and frequency of that word in that doc. for example, "random_word": [(1, 0.24) (99, 0.0029) ... ] where every tupple is (docID, frequency)'''
     try:
         filename = 'REPORT.txt'
+        file2 = 'InvertedIndex.txt'
 
         if os.path.isfile(filename):
             os.remove(filename)
 
+        if os.path.isfile(file2):
+            os.remove(file2)
+
         file = open(filename, 'w')
         file.write("REPORT: \n")
 
+        InvertedIndexTXT = open(file2, 'w')
+
+        file.write('Number of indexed documents: ' + str(docID) + '.\n')
+
+        file.write('Number of unique words: ' +
+                   str(len(inverted_index)) + '.\n')
+
         for token in inverted_index:
-            file.write(token + ": [ \n")
+            InvertedIndexTXT.write(token + ": [ \n")
             new_line_count = 0
             for post in inverted_index[token]:
-                file.write("(" + str(post.docId) +
-                           ", " + str(post.tfidf) + ') ')
+                InvertedIndexTXT.write("(" + str(post.docId) +
+                                       ", " + str(post.tfidf) + ') ')
                 new_line_count += 1
                 if new_line_count >= 10:
-                    file.write('\n')
+                    InvertedIndexTXT.write('\n')
                     new_line_count = 0
 
-            file.write('] \n------------------------------\n')
+            InvertedIndexTXT.write('] \n------------------------------\n')
 
+        file_path = 'path_to_your_file'  # Replace with the actual file path
+
+        file_size = os.path.getsize(file2)
+        file.write('Size of the inverted index: ' +
+                   str(file_size // 1024) + ' KB.\n')
         file.close()
+        InvertedIndexTXT.close()
         print('DONE')
     except Exception as e:
         print(
@@ -158,15 +177,19 @@ def launch_milestone_1():
     '''our main funciton.'''
     folder_path = '/home/mnadi/121/A3/search_engine/DEV'
     paths = get_file_paths(folder_path)  # list of paths to all the files
-    docID = 0
     for path in paths:
-        if docID >= 1000:
-            break
-        docID += 1
+        global docID
+        # if docID >= 1000:
+        #     break
+
         # text_content of file located at path
         text_content = get_file_text_content(path)
         if not text_content:  # skip if no text content
             continue
+
+        # if this file is a duplicate, continue ^^^
+
+        docID += 1
         # assign docID to its proper URL // {docID : url}
         map_docID_url(path, docID)
         tokens = tokenizer(text_content)  # tokenize the text content
