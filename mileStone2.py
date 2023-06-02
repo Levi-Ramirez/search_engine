d274d550ab5383c249fc88c9cb9706c5d333649e
Levi Arias Ramirez <leviar@circinus-48.ics.uci.edu>
2023-06-01 22:05:24 -0700

    n-grams (I believe) are working

--- mileStone2.py
+++ mileStone2.py
@@ -71,6 +71,66 @@ def read_large_line(file):
 
     return line
 
+
+#OLD BUT TESTED generate_boolean_search
+# def generate_boolean_search_result(boolean_query_list):
+
+#     try:
+#         search_result_docIDs = []
+#         # {'decemb': [[4, [1826, 1917], 2]]} => {word: [[docID, [positions...], frequency]]}
+#         if len(boolean_query_list) == 0:
+#             return search_result_docIDs #return the empty set
+#         least_seen_word_object = boolean_query_list[0]
+#         # print("least seen word: ", least_seen_word_object)
+#         least_seen_word = list(boolean_query_list[0].keys())[0]  # decemb
+#         # print("least_seen_word_object[least_seen_word]", least_seen_word_object[least_seen_word])
+#         # print(len(boolean_query_list))
+#         baseList = [] #baseList is the list of docID's that are in the least_seen_word
+#         for posting in least_seen_word_object[least_seen_word]:
+#                 baseList.append(posting[0])
+#         if len(boolean_query_list) == 1: #if its 1 query term, then write out the documents that have that term
+#             return baseList
+
+#         #filter base list
+
+#         # minTFIDF = 
+#         #first100Count = 100
+        
+#         print("this far")
+#         for curTerm in boolean_query_list: #for every term in the query list
+#             for token in curTerm: #should only be 1
+#                 i = 0
+#                 j = 0
+#                 while i < len(baseList): # for every element in the baselist
+#                     #print("i: ", i)
+#                     #print("baseList:", baseList)
+#                     while j < len(curTerm[token]): # for every posting in the current term
+#                         #print("j:", j)
+#                         curPosting = curTerm[token][j] #note: curTerm[token][j] gives a posting, curPosting[0] gives the docID
+#                         if curPosting[0] > baseList[i]: #if current posting's doc id is greater than the baseList's doc id
+#                             del baseList[i]
+#                             i -= 1 #decriment it because it will be incrimented later (an incriment + a delition will make it go forward two spots)
+#                             if len(baseList) == 0:
+#                                 return baseList #return empty list if empty
+#                             else:
+#                                 break #go to the next element in baselist (with the i+= below), keep current position in curTerm[token]
+#                         elif curPosting[0] == baseList[i]:
+#                             j += 1 #go to the next element in baselist (with the i+= below), incriment to next position in curTerm[token]
+#                             break
+#                         j += 1 #if nothing above, just incriment j to go to the next posting
+
+#                     i += 1
+    
+#         return baseList
+                        
+
+#     except Exception as e:
+#         print('ERROR in generate_boolean_search_result: ', str(e))
+#         return []
+
+
+
+#MOST RECENT MODIFICATIONS BELOW
 def generate_boolean_search_result(boolean_query_list):
 
     try:
@@ -84,10 +144,17 @@ def generate_boolean_search_result(boolean_query_list):
         # print("least_seen_word_object[least_seen_word]", least_seen_word_object[least_seen_word])
         # print(len(boolean_query_list))
         baseList = [] #baseList is the list of docID's that are in the least_seen_word
+        
         for posting in least_seen_word_object[least_seen_word]:
-                baseList.append(posting[0])
+                baseList.append([posting[0], posting[2]])
+                # docIDList.append(posting[0])
         if len(boolean_query_list) == 1: #if its 1 query term, then write out the documents that have that term
             return baseList
+
+        #filter base list
+
+        minTFIDF = 0
+        firstFewCount = 200
         
         print("this far")
         for curTerm in boolean_query_list: #for every term in the query list
@@ -97,36 +164,132 @@ def generate_boolean_search_result(boolean_query_list):
                 while i < len(baseList): # for every element in the baselist
                     #print("i: ", i)
                     #print("baseList:", baseList)
+
+                    #calculate new minimum tf-idf to look for up until the "firstFewCount" runs out
+                    if firstFewCount > 0:
+                        firstFewCount -= 1
+                        if minTFIDF > baseList[i][1]: #compare current minimum with the base list
+                            minTFIDF = baseList[i][1]
+                    else:
+                        if minTFIDF > baseList[i][1]:
+                            print("deletes...")
+                            del baseList[i]
+                            continue
+
                     while j < len(curTerm[token]): # for every posting in the current term
                         #print("j:", j)
                         curPosting = curTerm[token][j] #note: curTerm[token][j] gives a posting, curPosting[0] gives the docID
-                        if curPosting[0] > baseList[i]: 
+                        if curPosting[0] > baseList[i][0]: #if current posting's doc id is greater than the baseList's doc id
                             del baseList[i]
                             i -= 1 #decriment it because it will be incrimented later (an incriment + a delition will make it go forward two spots)
                             if len(baseList) == 0:
                                 return baseList #return empty list if empty
                             else:
                                 break #go to the next element in baselist (with the i+= below), keep current position in curTerm[token]
-                        elif curPosting[0] == baseList[i]:
+                        elif curPosting[0] == baseList[i][0]: #if current posting has the same docID, add it to the list and add to baseList docID's tf-idf
+                            baseList[i][1] += curPosting[2]
                             j += 1 #go to the next element in baselist (with the i+= below), incriment to next position in curTerm[token]
                             break
                         j += 1 #if nothing above, just incriment j to go to the next posting
 
                     i += 1
-    
+        # print("HEREEEE")
+        # print("baseList: ", baseList)
+        # docIDList = []
+        # for x in baseList:
+        #     docIDList.append(x[0])
+        # print("docIDList: ", docIDList)
+        # print("baseList: ", baseList)
+
+        baseList.sort(key = lambda x: x[1], reverse = True) #sort according to tfIDF
+        if len(baseList) > 500: #get the top 500 documents
+            baseList = baseList[:500]
         return baseList
+
+        # return docIDList
                         
 
     except Exception as e:
         print('ERROR in generate_boolean_search_result: ', str(e))
         return []
 
-def docOrder(query_docs, boolean_query_list): #query term list, query document result
+
+
+# def generate_boolean_search_result(boolean_query_list):
+
+#     try:
+#         search_result_docIDs = []
+#         # {'decemb': [[4, [1826, 1917], 2]]} => {word: [[docID, [positions...], frequency]]}
+#         if len(boolean_query_list) == 0:
+#             return search_result_docIDs #return the empty set
+#         least_seen_word_object = boolean_query_list[0]
+#         # print("least seen word: ", least_seen_word_object)
+#         least_seen_word = list(boolean_query_list[0].keys())[0]  # decemb
+#         # print("least_seen_word_object[least_seen_word]", least_seen_word_object[least_seen_word])
+#         # print(len(boolean_query_list))
+#         baseList = [] #baseList is the list of docID's and tf-idfID's that are in the least_seen_word
+#         for posting in least_seen_word_object[least_seen_word]:
+#                 baseList.append([posting[0], posting[2]])
+#         if len(boolean_query_list) == 1: #if its 1 query term, then write out the documents that have that term
+#             return baseList
+
+#         #filter base list
+
+#         minTFIDF = 0
+#         first200Count = 200 # threshold
+
+        
+#         print("this far")
+#         for curTerm in boolean_query_list: #for every term in the query list
+#             for token in curTerm: #should only be 1
+#                 i = 0
+#                 j = 0
+#                 while i < len(baseList): # for every element in the baselist
+
+#                     # #calculate new minimum tf-idf to look for
+#                     # if first200Count > 0:
+#                     #     first200Count -= 1
+#                     #     if minTFIDF > baseList[i][1]: #compare current minimum with the base list
+#                     #         minTFIDF = baseList[i][1]
+#                     # else:
+#                     #     if minTFIDF > baseList[i][1]:
+#                     #         del baseList[i]
+#                     #         continue
+
+#                     #print("i: ", i)
+#                     #print("baseList:", baseList)
+#                     while j < len(curTerm[token]): # for every posting in the current term
+#                         #print("j:", j)
+#                         curPosting = curTerm[token][j] #note: curTerm[token][j] gives a posting, curPosting[0] gives the docID
+#                         if curPosting[0] > baseList[i][0]: #if current posting's doc id is greater than the baseList's doc id
+#                             del baseList[i]
+#                             i -= 1 #decriment it because it will be incrimented later (an incriment + a delition will make it go forward two spots)
+#                             if len(baseList) == 0:
+#                                 return baseList #return empty list if empty
+#                             else:
+#                                 break #go to the next element in baselist (with the i+= below), keep current position in curTerm[token]
+#                         elif curPosting[0] == baseList[i][0]:
+#                             j += 1 #go to the next element in baselist (with the i+= below), incriment to next position in curTerm[token]
+#                             break
+#                         j += 1 #if nothing above, just incriment j to go to the next posting
+
+#                     i += 1
+    
+#         return baseList
+                        
+
+    except Exception as e:
+        print('ERROR in generate_boolean_search_result: ', str(e))
+        return []
+
+def docOrder(query_docsID_tfidf, boolean_query_list): #query term list, query document result
     '''returns a list of lists [[docID-1, tfidf-1], [docID-2, tfidf-2]] in order of decreasing tfidf'''
     doc_ngram_count = []
-    for docID in query_docs:
+    for docID_tfidf in query_docsID_tfidf:
+        docID = docID_tfidf[0]
+        tfidf = docID_tfidf[1]
         partial_ngrams = nGramDoc(docID, boolean_query_list) #returns an int of the number of partial ngrams found
-        doc_ngram_count.append([docID, partial_ngrams])
+        doc_ngram_count.append([docID, partial_ngrams + tfidf])
     return doc_ngram_count
 
 
@@ -168,6 +331,7 @@ def nGramDoc(docID, boolean_query_list):
             if(len(tokenPosInDoc[minIndex]) <= indexPos[minIndex]): #if it reached the end of this list, break cuz the query n-gram is not not relavent
                 break
             minIndex = getMinIndex(indexPos, tokenPosInDoc)
+    #return count, tfidfScore
     return count
 
         
@@ -192,7 +356,8 @@ def launch_milestone_2():
 
     try:
         # index_of_index_shelve = shelve.open("index_of_index.shelve")
-        index_of_index = json.load(open("index_of_index.txt"))
+        # index_of_index = json.load(open("index_of_index.txt"))
+        index_of_index = json.load(open("index_of_index_tf_idf.txt"))
         docId_to_urls = json.load(open('docID_urls.txt'))
         # print('docId_to_urls', docId_to_gurls['data'])
         # print('index_of_inverted_index', index_of_inverted_index)
@@ -204,7 +369,9 @@ def launch_milestone_2():
         boolean_query_list = []
         # term_being_searched: [post] => [{'decemb': [[4, [1826, 1917], 2]]}, {'deng': [[4, [222], 1]]}, {'depart': [[4, [2687], 1], [9, [108], 1], [12, [84], 1]]}]
 
-        full_index = open('full_index.txt', 'r')
+        # full_index = open('full_index.txt', 'r')
+        full_index = open('full_index_tf_idf.txt', 'r')
+
 
         print(query_tokens)
         # print('index_of_index', index_of_index)
@@ -231,16 +398,17 @@ def launch_milestone_2():
             else: #if not in index_of_index
                 boolean_query_list = [] #set it to an empty list, because this means it found a word that wasn't in any of the documents
 
-        boolean_query_list = sorted(
+        boolean_query_list_sorted = sorted(
             boolean_query_list, key=lambda x: len(list(x.values())[0]))
         # [{'decemb': [[4, [1826, 1917], 2]]}, {'deng': [[4, [222], 1]]}, {'depart': [[4, [2687], 1], [9, [108], 1], [12, [84], 1]]}]
         #print(boolean_query_list)
         print("bool_query_list len: ", len(boolean_query_list))
         #print("bool_query_list: ", boolean_query_list)
-        query_docs = generate_boolean_search_result(boolean_query_list) #res is the docID's which gets all of the docs that contain the terms in the boolean_query_list
+        query_docsID_tfidf = generate_boolean_search_result(boolean_query_list_sorted) #res is the docID's which gets all of the docs that contain the terms in the boolean_query_list
         #nGram the list and return them and their weights by assorted order
-        print(docOrder(query_docs, boolean_query_list)) #query term list, query document result
-        if len(query_docs) == 0:
+
+        print(docOrder(query_docsID_tfidf, boolean_query_list)) #query term list, query document result
+        if len(query_docsID_tfidf) == 0:
             print('SEARCH RESULT ==> No search results containing all query terms')
         else:
             print("nothing")
